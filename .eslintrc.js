891e6a2b8d2479148c3d04a380c1a43bb0eecf1c
Fran Guijarro <franleplant@gmail.com>
2021-12-12 23:06:27 -0300

    initial commit'

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // TODO make this only for tests
+    "no-unused-expressions": [0],
+  },
+};
