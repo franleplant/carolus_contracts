c34ee5c904b7ba9bc2d3e4f71baf07606afc7aa8
Fran Guijarro <franleplant@gmail.com>
2022-01-23 15:24:53 -0300

    cleanup

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,15 +6,20 @@
 import { ethers } from "hardhat";
 import Env from "../lib/Env";
 import DeployInfo from "../lib/deployInfo";
-import { range } from "lodash";
-import { ContractTransaction } from "@ethersproject/contracts";
-import isTxOk from "../lib/isTxOk";
+
+import {BigNumberish} from "@ethersproject/bignumber"
+
 
 const TOKEN_NAME = Env.get("TOKEN_NAME");
 const TOKEN_SYMBOL = Env.get("TOKEN_SYMBOL");
-const TOKEN_MINT_PRICE = ethers.constants.WeiPerEther.div(
-  1 / Env.get("TOKEN_MINT_PRICE", "number")
-);
+// TODO
+const MINT_PRICE_RAW = Env.get("TOKEN_MINT_PRICE", "number")
+let TOKEN_MINT_PRICE: BigNumberish;
+if (MINT_PRICE_RAW < 1) {
+  TOKEN_MINT_PRICE = ethers.constants.WeiPerEther.div(1 / MINT_PRICE_RAW);
+} else {
+  TOKEN_MINT_PRICE = ethers.constants.WeiPerEther.mul(MINT_PRICE_RAW);
+}
 console.log(
   "USING MINT PRICE",
   TOKEN_MINT_PRICE,
@@ -23,7 +28,6 @@ console.log(
 const TOKEN_BASE_URI = Env.get("TOKEN_URI");
 console.log("USIGN TOKEN URI", TOKEN_BASE_URI);
 
-//const CONTENT_1 = "This is a fake content piece";
 
 async function main() {
   const ContractFactory = await ethers.getContractFactory("CarolusNFTV1");
@@ -50,19 +54,6 @@ async function main() {
   await DeployInfo.write({ address: contract.address });
   console.log("Contract deployed to:", contract.address);
 
-  //await Promise.all(
-  //range(10).map((index) => {
-  //const tx = contract.publishMint(
-  //`${index.toString().padStart(3, "0")} ${CONTENT_1}`,
-  //{
-  //value: TOKEN_MINT_PRICE,
-  //gasLimit: 300000,
-  //}
-  //);
-
-  //return isTxOk(tx);
-  //})
-  //);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
